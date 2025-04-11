f1ba2f32b947efc214fc1a3e84c5ffa4849f6963
nathanrodriguees <nathan_rodrigues07@outlook.com>
2025-04-11 10:43:15 -0300

    feat: Desafio sintaxe

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+		Scanner txt = new Scanner(System.in);
+		Scanner num = new Scanner(System.in);
+
+		int numero;
+		String agencia;
+		String nome;
+		float saldo;
+
+		System.out.println("Digite o número da conta: ");
+		numero = num.nextInt();
+
+		System.out.println("Digite o número da agência: ");
+		agencia = txt.nextLine();
+
+		System.out.println("Digite o seu nome: ");
+		nome = txt.nextLine();
+
+		System.out.println("Digite o seu saldo: ");
+		saldo = num.nextFloat();
+
+		System.out.printf("Olá " + nome + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia
+				+ ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
+
+	}
+}
